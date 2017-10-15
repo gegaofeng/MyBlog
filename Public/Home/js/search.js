658a816767f5cbdbdbc6c45937ac2fa81f32268e
ge <ge@ge>
2017-10-15 10:13:23 +0800

    000

--- Public/Home/js/search.js
+++
@@ -1,9 +0,0 @@
-/**
- * Created by Administrator on 16-1-31.
- */
-$(function(){
-    $("#do").on("click",function(){
-        var word = $("#word").val();
-        window.location.href = "/index.php?s=/Home/article/search/"+word;
-    });
-})
